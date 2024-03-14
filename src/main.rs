92c0a283b97e09801ff13da5f6a7f3b7bcb67ea9
loberdesp <siembab04@gmail.com>
2024-03-14 02:17:06 +0100

    final version without comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use reqwest;
-use serde::{Serialize, Deserialize};
-use std::io;
-use std::io::Write;
-use std::time::Duration;
+use reqwest; 
+use serde::{Serialize, Deserialize}; 
+use std::io; 
+use std::io::Write; 
+use std::time::Duration; 
 use tokio::time::sleep;
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ExchangeRates {
     conversion_rates: std::collections::HashMap<String, f64>,
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ErrorResponse {
@@ -19,30 +17,25 @@ struct ErrorResponse {
     error_type: String,
 }
 
-
 #[derive(Debug, Serialize, Deserialize)]
 struct SupportedList {
     supported_codes: Vec<Vec<String>>,
 }
 
+async fn display_currencies(map: &mut std::collections::HashMap<String, std::collections::HashMap<String, f64>>) -> Result<(), reqwest::Error> {
 
-async fn display_currencies(m: &mut Option<std::collections::HashMap<String, std::collections::HashMap<String, f64>>>) -> Result<(), reqwest::Error> {
-    let list_link = "https://v6.exchangerate-api.com/v6/a3f798577a713b0309d32d40/latest/USD";
+    if let Some(usd_map) = map.get("USD") {
+        for (key, value) in usd_map {
+            println!("Code: {}, Rate: {}", key, value);
+        }
+    } else {
+        let list_link = "https://v6.exchangerate-api.com/v6/a3f798577a713b0309d32d40/latest/USD";
         let list_response = reqwest::Client::new().get(list_link).send().await;
-
-        
-
-
         match list_response {
             Ok(res) => {
                 if res.status().is_success() {
                     let list_body: ExchangeRates = res.json().await?;
-
-                    if let Some(map) = m {
-                        map.insert("USD".to_string(), list_body.conversion_rates.clone());
-                    }
-
-
+                    map.insert("USD".to_string(), list_body.conversion_rates.clone());
                     for (key, value) in list_body.conversion_rates {
                         println!("Code: {}, Rate: {}", key, value);
                     }
@@ -52,6 +45,7 @@ async fn display_currencies(m: &mut Option<std::collections::HashMap<String, std
                 eprintln!("Failed to parse currencies: {}", err);
             }
         }
+    }
     Ok(())
 }
 
@@ -62,27 +56,26 @@ fn read_value() -> f64 {
     io::stdin()
         .read_line(&mut input_line)
         .expect("Failed to read line");
-
     let value: f64 = input_line
         .trim()
         .parse()
-        .expect("Input not an integer");
-
-    if value<=0.0 {
-        println!("Value less or equal to 0, enter valid value");
+        .expect("Input not a valid floating-point number");
+    if value <= 0.0 {
+        println!("Value less than or equal to 0, enter a valid value");
         return -1.0;
     } else {
         return value;
-    } 
+    }
 }
 
 fn is_uppercase(input: &str) -> bool {
     input.chars().all(char::is_uppercase)
 }
 
-async fn read_input_code(available_currencies: &mut Option<std::collections::HashMap<String, std::collections::HashMap<String, f64>>>) -> Result<(), reqwest::Error> {
-
+async fn read_input_code(available_currencies: &mut std::collections::HashMap<String, std::collections::HashMap<String, f64>>) -> Result<(), reqwest::Error> {
     let mut cur_from = String::new();
+    let mut cur_to = String::new();
+
     loop {
         print!("Enter base currency (e.g., USD): ");
         io::stdout().flush().expect("Failed to flush base currency");
@@ -92,13 +85,11 @@ async fn read_input_code(available_currencies: &mut Option<std::collections::Has
         if is_uppercase(&cur_from) {
             break;
         } else {
-            println!("Invalid input. It should only contain upper case characters!");
+            println!("Invalid input. It should only contain uppercase characters!");
             cur_from.clear();
         }
     }
-    
 
-    let mut cur_to = String::new();
     loop {
         print!("Enter output currency (e.g., GBP): ");
         io::stdout().flush().expect("Failed to flush output currency");
@@ -108,99 +99,45 @@ async fn read_input_code(available_currencies: &mut Option<std::collections::Has
         if is_uppercase(&cur_to) {
             break;
         } else {
-            println!("Invalid input. It should only contain upper case characters!");
+            println!("Invalid input. It should only contain uppercase characters!");
             cur_to.clear();
         }
     }
-    
-    if let Some(currency_rates) = available_currencies {
 
-        // for (key, _value) in currency_rates.clone() {
-        //     println!("Key: {}", key);
-        // }
-
-        //nie dodaje sie klucz
-
-        // if currency_rates.contains_key(&cur_from) {
-        //     println!("JEST");
-        // } else {
-        //     println!("nie ma :(");
-        // }
-
-        let num = read_value();
-        if num != -1.0 {
-            if let Some(rate_from) = currency_rates.get(&cur_from) {
-                if let Some(rate_to) = rate_from.get(&cur_to) {
-                    non_api_convert(cur_from.to_string(), cur_to.to_string(), num, *rate_to);
-                    println!("got it without api!");
-                } else {
-                    api_convert(cur_from.to_string(), cur_to.to_string(), num, currency_rates).await?;
-                    println!("got it with api 1 :(");
-                }
+    let num = read_value();
+    if num != -1.0 {
+        if let Some(rate_from) = available_currencies.get(&cur_from) {
+            if let Some(rate_to) = rate_from.get(&cur_to) {
+                non_api_convert(cur_from.to_string(), cur_to.to_string(), num, *rate_to);
             } else {
-                api_convert(cur_from.to_string(), cur_to.to_string(), num, currency_rates).await?;
-                println!("got it with api 2 :(");
+                api_convert(cur_from.to_string(), cur_to.to_string(), num, available_currencies).await?;
             }
+        } else {
+            api_convert(cur_from.to_string(), cur_to.to_string(), num, available_currencies).await?;
         }
     }
     Ok(())
 }
 
-
 fn non_api_convert(from: String, to: String, amount: f64, rate: f64) {
-    println!("{:.2} {} exchanged with {} rate is {:.2} {}", amount, &from, rate, amount*rate, &to);
+    println!("{:.2} {} exchanged with {} rate is {:.2} {}", amount, &from, rate, amount * rate, &to);
 }
 
-
 async fn api_convert(from: String, to: String, amount: f64, map: &mut std::collections::HashMap<String, std::collections::HashMap<String, f64>>) -> Result<(), reqwest::Error> {
-
     let base = "https://v6.exchangerate-api.com/v6/a3f798577a713b0309d32d40/latest/".to_string();
-
     let link = base + &from;
     let mut retry_counter = 0;
 
     loop {
         let response = reqwest::Client::new().get(&link).send().await;
 
-        
-
         match response {
             Ok(res) => {
                 if res.status().is_success() {
-
-                    // if let Some(inner_map) = map.as_ref().and_then(|m| m.get(&from)) {
-                    //     println!("inside, no need to add, can call non_api function");
-
-                    // } else {
-                    //     println!("not inside, gotta add it to the map");
-                    //     let mut new_map: std::collections::HashMap<String, std::collections::HashMap<String, f64>> = std::collections::HashMap::new();
-                    //     map.insert(new_map);
-                    // }
-
-                    
                     let body: ExchangeRates = res.json().await?;
-
-
                     let _ = map.insert(from.clone(), body.conversion_rates.clone());
-
-                    // println!("Inserted {} exchange hashmap", from.clone());
-
-                    
-                    // if let Some(m) = map.as_mut() {
-                    //     for (key, _inner_map) in m {
-                    //         println!("Outer Key: {}", key);
-                    //     }
-                    // }
-
-
-
-                    
-
-                    
-                    
-
                     if let Some(rate) = body.conversion_rates.get(&to) {
-                        println!("{:.2} {} exchanged with {} rate is {:.2} {}", amount, &from, rate, amount*rate, &to);
+                        println!("{:.2} {} exchanged with {} rate is {:.2} {}", amount, &from, rate, amount * rate, &to);
                         break;
                     } else {
                         println!("Error: Invalid output currency: {}", &to);
@@ -208,25 +145,21 @@ async fn api_convert(from: String, to: String, amount: f64, map: &mut std::colle
                     }
                 } else if res.status() == reqwest::StatusCode::TOO_MANY_REQUESTS {
                     eprintln!("Rate limit exceeded. Retrying after delay...");
-
-                    // Wait for a moment before retrying (you may adjust the duration)
                     sleep(Duration::from_secs(5)).await;
-
-                    // Increment retry counter and check if exceeded maximum retries
                     retry_counter += 1;
                     if retry_counter > 3 {
                         eprintln!("Maximum retries reached. Exiting.");
                         return Ok(());
                     }
                 } else {
-                    let error_body : ErrorResponse = res.json().await?;
+                    let error_body: ErrorResponse = res.json().await?;
                     match error_body.error_type.as_ref() {
-                        "unsupported-code" =>println!("Error: Invalid input currency code: {}", &from),
-                        "malformed-request" =>println!("Error: Invalid request structure"),
-                        "invalid-key" =>println!("Error: Invalid API key"),
-                        "inactive-account" =>println!("Error: Inactive account (email address not confirmed)"),
-                        "quota-reached" =>println!("Error: Limit of account's requests exceeded"),
-                        _=>println!("Error: Unknown error code"),
+                        "unsupported-code" => println!("Error: Invalid input currency code: {}", &from),
+                        "malformed-request" => println!("Error: Invalid request structure"),
+                        "invalid-key" => println!("Error: Invalid API key"),
+                        "inactive-account" => println!("Error: Inactive account (email address not confirmed)"),
+                        "quota-reached" => println!("Error: Limit of account's requests exceeded"),
+                        _ => println!("Error: Unknown error code"),
                     }
                     return Ok(());
                 }
@@ -240,12 +173,9 @@ async fn api_convert(from: String, to: String, amount: f64, map: &mut std::colle
     Ok(())
 }
 
-
-
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-
-    let mut curs: Option<std::collections::HashMap<String, std::collections::HashMap<String, f64>>> = Some(std::collections::HashMap::new());
+    let mut curs: std::collections::HashMap<String, std::collections::HashMap<String, f64>> = std::collections::HashMap::new();
 
     loop {
         println!("------------------------------- MENU -------------------------------");
@@ -254,7 +184,7 @@ async fn main() -> Result<(), reqwest::Error> {
         println!("2 - Exit program");
         print!("Enter option: ");
         io::stdout().flush().expect("Failed to flush menu");
-        
+
         let mut menu = String::new();
         io::stdin()
             .read_line(&mut menu)
@@ -265,10 +195,8 @@ async fn main() -> Result<(), reqwest::Error> {
             .parse()
             .expect("Input isn't a valid menu option");
 
-
         match m_value {
             0 => {
-                //curs = Some(display_currencies().await?);
                 display_currencies(&mut curs).await?;
             }
             1 => {
@@ -282,12 +210,12 @@ async fn main() -> Result<(), reqwest::Error> {
                 println!("Input isn't a valid menu option");
             }
         }
-        
-        if let Some(m) = &curs {
-            for (key, _inner_map) in m {
-                println!("Keys after one loop: {}", key);
-            }
-        }
+
+        //debug purpose code below, display all cached exchanged rates to optimize and reduce api calls
+
+        // for(key, _value) in &curs {
+        //     println!("{:?}", key)
+        // }
     }
     Ok(())
 }
