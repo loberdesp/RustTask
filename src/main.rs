ad5f42abe8a557a4232a962ee591936a7f50f843
loberdesp <siembab04@gmail.com>
2024-03-14 03:54:38 +0100

    documentation addded

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::io::Write; // Importing Write trait for flushing output.
 use std::time::Duration; // Importing Duration for specifying time durations.
 use tokio::time::sleep; // Importing sleep function from tokio for asynchronous sleep.
 
+static API_KEY: &str = "a3f798577a713b0309d32d40";
+
 #[derive(Debug, Serialize, Deserialize)] // Implementing Serialize and Deserialize traits for ExchangeRates struct.
 struct ExchangeRates {
     conversion_rates: std::collections::HashMap<String, f64>, // Struct representing exchange rates.
@@ -26,22 +28,27 @@ async fn display_currencies(map: &mut std::collections::HashMap<String, std::col
     // Function to display available currencies and their exchange rates.
     if let Some(usd_map) = map.get("USD") { // Checking if USD exchange rates are already available.
         for (key, value) in usd_map {
-            println!("Code: {}, Rate: {}", key, value); // Printing currency code and exchange rate.
+            println!("Code: {}, Rate: 1 USD is {} {}", key, value, key); // Printing currency code and exchange rate.
         }
     } else {
-        let list_link = "https://v6.exchangerate-api.com/v6/a3f798577a713b0309d32d40/latest/USD"; // API endpoint for getting currency exchange rates.
-        let list_response = reqwest::Client::new().get(list_link).send().await; // Sending HTTP GET request to get exchange rates.
+        let base = "https://v6.exchangerate-api.com/v6/".to_string(); // API endpoint for getting currency exchange rates.
+        let end = "/latest/USD".to_string();
+
+        let list_link = base + &API_KEY + &end;
+
+        let list_response = reqwest::Client::new().get(&list_link).send().await; // Sending HTTP GET request to get exchange rates.
         match list_response {
             Ok(res) => {
                 if res.status().is_success() { // Checking if the request was successful.
                     let list_body: ExchangeRates = res.json().await?; // Parsing JSON response into ExchangeRates struct.
                     map.insert("USD".to_string(), list_body.conversion_rates.clone()); // Inserting USD exchange rates into the map.
                     for (key, value) in list_body.conversion_rates {
-                        println!("Code: {}, Rate: {}", key, value); // Printing currency code and exchange rate.
+                        println!("Code: {}, Rate: 1 USD is {} {}", key, value, key); // Printing currency code and exchange rate.
                     }
                 }
             }
             Err(err) => {
+                println!("");
                 eprintln!("Failed to parse currencies: {}", err); // Printing error if failed to parse currencies.
             }
         }
@@ -62,6 +69,7 @@ fn read_value() -> f64 {
         .parse()
         .expect("Input not a valid floating-point number"); // Parsing user input to a floating-point number.
     if value <= 0.0 {
+        println!("");
         println!("Value less than or equal to 0, enter a valid value"); // Printing error message for invalid input.
         return -1.0;
     } else {
@@ -88,7 +96,9 @@ async fn read_input_code(available_currencies: &mut std::collections::HashMap<St
         if is_uppercase(&cur_from) { // Checking if input currency code is uppercase.
             break; // Exiting loop if input is valid.
         } else {
+            println!("");
             println!("Invalid input. It should only contain uppercase characters!"); // Printing error message for invalid input.
+            println!("");
             cur_from.clear(); // Clearing the input string.
         }
     }
@@ -102,7 +112,9 @@ async fn read_input_code(available_currencies: &mut std::collections::HashMap<St
         if is_uppercase(&cur_to) { // Checking if output currency code is uppercase.
             break; // Exiting loop if input is valid.
         } else {
+            println!("");
             println!("Invalid input. It should only contain uppercase characters!"); // Printing error message for invalid input.
+            println!("");
             cur_to.clear(); // Clearing the input string.
         }
     }
@@ -125,16 +137,21 @@ async fn read_input_code(available_currencies: &mut std::collections::HashMap<St
 fn non_api_convert(from: String, to: String, amount: f64, rate: f64) {
     // Function to perform conversion without using API.
     println!("");
+    println!("");
     println!("#####################################################################");
     println!("{:.2} {} exchanged with {} rate is {:.2} {}", amount, &from, rate, amount * rate, &to); // Printing conversion result.
     println!("#####################################################################");
-    println!("");
+
+
 }
 
 async fn api_convert(from: String, to: String, amount: f64, map: &mut std::collections::HashMap<String, std::collections::HashMap<String, f64>>) -> Result<(), reqwest::Error> {
     // Function to perform conversion using API.
-    let base = "https://v6.exchangerate-api.com/v6/a3f798577a713b0309d32d40/latest/".to_string(); // API base URL.
-    let link = base + &from; // Creating API endpoint URL.
+    let base = "https://v6.exchangerate-api.com/v6/".to_string(); // API base URL.
+    let end = "/latest/".to_string();
+
+    let link = base + &API_KEY + &end + &from; // Creating API endpoint URL.
+
     let mut retry_counter = 0; // Initializing retry counter.
 
     loop {
@@ -147,13 +164,17 @@ async fn api_convert(from: String, to: String, amount: f64, map: &mut std::colle
                     let _ = map.insert(from.clone(), body.conversion_rates.clone()); // Inserting exchange rates into the map.
                     if let Some(rate) = body.conversion_rates.get(&to) { // Checking if exchange rate for output currency is available.
                         println!("");
+                        println!("");
                         println!("#####################################################################");
                         println!("{:.2} {} exchanged with {} rate is {:.2} {}", amount, &from, rate, amount * rate, &to); // Printing conversion result.
                         println!("#####################################################################");
-                        println!("");
+
+
                         break; // Exiting loop.
                     } else {
+                        println!("");
                         println!("Error: Invalid output currency: {}", &to); // Printing error message for invalid output currency.
+                        println!("");
                         break; // Exiting loop.
                     }
                 } else if res.status() == reqwest::StatusCode::TOO_MANY_REQUESTS { // Checking if rate limit exceeded.
@@ -165,6 +186,7 @@ async fn api_convert(from: String, to: String, amount: f64, map: &mut std::colle
                         return Ok(());
                     }
                 } else {
+                    println!("");
                     let error_body: ErrorResponse = res.json().await?; // Parsing JSON error response into ErrorResponse struct.
                     match error_body.error_type.as_ref() { // Matching error type.
                         "unsupported-code" => println!("Error: Invalid input currency code: {}", &from),
@@ -174,10 +196,12 @@ async fn api_convert(from: String, to: String, amount: f64, map: &mut std::colle
                         "quota-reached" => println!("Error: Limit of account's requests exceeded"),
                         _ => println!("Error: Unknown error code"),
                     }
+                    println!("");
                     return Ok(());
                 }
             }
             Err(_err) => {
+                println!("");
                 eprintln!("Error: Network error"); // Printing network error message.
                 return Ok(());
             }
@@ -192,6 +216,8 @@ async fn main() -> Result<(), reqwest::Error> {
     let mut curs: std::collections::HashMap<String, std::collections::HashMap<String, f64>> = std::collections::HashMap::new(); // Initializing HashMap to store exchange rates.
 
     loop {
+        println!("");
+        println!("");
         println!("------------------------------- MENU -------------------------------");
         println!("0 - List all available currencies and exchange rates (for US dollar)");
         println!("1 - Enter base currency (the one you convert from)");
@@ -217,10 +243,13 @@ async fn main() -> Result<(), reqwest::Error> {
                 read_input_code(&mut curs).await?; // Reading input currency codes and performing conversion.
             }
             2 => {
+                println!("");
                 println!("Exiting program!"); // Printing exit message.
+                println!("");
                 break; // Exiting loop.
             }
             _ => {
+                println!("");
                 println!("Input isn't a valid menu option"); // Printing error message for invalid menu option.
             }
         }
